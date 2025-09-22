045220f332f08e07dc1a6e85c618450ead262949
Ton Nom <louiscollard2@gmail.com>
2025-09-22 13:47:13 +0200

    ✨ F06: NAVBAR > Add ToggleButton and ToastAdmin

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
 import styled from "styled-components";
+import ToggleButton from "../../../shared/ToggleButton";
 import Profile from "./Profile";
+import ToastAdmin from "./ToastAdmin";
 
 export default function NavbarRightSide({ username }) {
+    const [isModeAdmin, setIsModeAdmin] = useState(false);
+
+    const displayToastNotification = () => {
+        if (!isModeAdmin) {
+            toast.info("Mode admin activé", {
+                theme: "dark",
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+        setIsModeAdmin(!isModeAdmin)
+    }
+
     return (
         <NavbarRightSideStyled>
-            {/* <div className="admin-button">Admin Button</div> */}
+            <ToggleButton
+                labelIfUnchecked="ACTIVER LE MODE ADMIN"
+                labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+                onToggle={displayToastNotification}
+            />
             <Profile username={username} />
-        </NavbarRightSideStyled>
+            <ToastAdmin />
+        </NavbarRightSideStyled >
     )
 }
 
@@ -14,12 +41,4 @@ const NavbarRightSideStyled = styled.div`
     display: flex;
     align-items: center;
     padding-right: 50px;
-
-    /* .admin-button {
-        background: lightblue;
-    } */
-
-    .profile {
-        background: yellow;
-    }
 `;
