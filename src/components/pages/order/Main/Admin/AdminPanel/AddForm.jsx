531b347685bf5d2be253fd5be1768e455acc2173
Ton Nom <louiscollard2@gmail.com>
2025-09-29 10:50:42 +0200

    🚧  F08: ADMIN > AddForm > Add inputs layouts

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,6 +1,42 @@
+import styled from "styled-components";
 
 export default function AddForm() {
     return (
-        <div>AddForm</div>
+        <AddFormStyled>
+            <div className="image-preview">ImagePreview</div>
+            <div className="input-fields">
+                <input type="text" placeholder="Nom" />
+                <input type="text" placeholder="Image URL" />
+                <input type="text" placeholder="Prix" />
+            </div>
+            <div className="submit-button">Submit button</div>
+        </AddFormStyled>
     )
 }
+
+const AddFormStyled = styled.div`
+    border: 2px solid black;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    height: 100%;
+    width: 70%;
+
+    .image-preview {
+        background: red;
+        grid-area: 1 / 1 / 4 / 2;
+    }
+
+    .input-fields {
+        background: blue;
+        grid-area: 1 / 2 / -2 / -1;
+
+        display: grid;
+    }
+
+    .submit-button {
+        background: green;
+        grid-area: 4 / 2 / -2 / -2;
+        width: 50%;
+    }
+`;
