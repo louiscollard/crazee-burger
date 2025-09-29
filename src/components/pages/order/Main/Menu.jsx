952d40eff4c72495918a49d75bfb4a2325c659ed
Ton Nom <louiscollard2@gmail.com>
2025-09-29 09:31:23 +0200

    🔧 F08: Set up files to start

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { fakeMenu } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
 import { formatPrice } from "../../../../utils/math";
 import Card from "../../../shared/Card";
 
 export default function Menu() {
-    const [menu, setMenu] = useState(fakeMenu2)
+    const [menu, setMenu] = useState(fakeMenu.MEDIUM)
 
     return (
         <MenuStyled className="menu">
