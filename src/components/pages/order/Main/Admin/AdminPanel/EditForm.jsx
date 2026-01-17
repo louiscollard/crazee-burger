59936fd774c40d8d6f16c354cf5d56c79e1a547e
Ton Nom <louiscollard2@gmail.com>
2026-01-17 23:12:17 +0100

    🚧 F09: ADMIN > Refactor EditForm component with styled-components and update layout

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,6 +1,32 @@
+import { HiCursorClick } from "react-icons/hi";
+import styled from "styled-components";
+import { theme } from "../../../../../../theme";
 
 export default function EditForm() {
     return (
-        <div>EditForm</div>
+        <EditFormStyled>
+            <p>Cliquer sur un produit pour le modifier</p>
+            <HiCursorClick className="icon" />
+        </EditFormStyled>
     )
 }
+
+const EditFormStyled = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100%;
+    
+    p {
+        font-family: "Amatic SC", cursive;
+        font-size: ${theme.fonts.size.P3};
+        color: ${theme.colors.greyMedium};
+    }
+
+    .icon {
+        margin-left: ${theme.spacing.xs};
+        font-size: ${theme.fonts.size.P3};
+        color: ${theme.colors.greyDark};
+    }
+`;
