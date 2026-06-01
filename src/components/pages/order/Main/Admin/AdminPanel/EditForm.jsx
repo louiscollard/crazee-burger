2d65ab43bf30aa70c559becc37c9ab9eab02b240
Ton Nom <louiscollard2@gmail.com>
2026-06-01 09:43:50 +0200

    🚧 F09: ADMIN > Refactor EditForm to use HintMessage component for improved clarity

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,32 +1,7 @@
-import { HiCursorClick } from "react-icons/hi";
-import styled from "styled-components";
-import { theme } from "../../../../../../theme";
+import HintMessage from "./HintMessage";
 
 export default function EditForm() {
     return (
-        <EditFormStyled>
-            <p>Cliquer sur un produit pour le modifier</p>
-            <HiCursorClick className="icon" />
-        </EditFormStyled>
+        <HintMessage />
     )
 }
-
-const EditFormStyled = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    height: 100%;
-    
-    p {
-        font-family: "Amatic SC", cursive;
-        font-size: ${theme.fonts.size.P3};
-        color: ${theme.colors.greyMedium};
-    }
-
-    .icon {
-        margin-left: ${theme.spacing.xs};
-        font-size: ${theme.fonts.size.P3};
-        color: ${theme.colors.greyDark};
-    }
-`;
